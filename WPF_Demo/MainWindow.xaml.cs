858108006cce5741f19b815e44baf6c427f2a388
monomarine <gamedev409@yandex.ru>
2024-05-28 12:15:51 +0500

    Добавьте файлы проекта.

---
+++ WPF_Demo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// конструктор по умолчанию для окна
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// нажатие на кнопку "закрыть окно"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //просмотр всего списка окон в текущем приложении
+            foreach (Window window in App.Current.Windows)
+            {
+                //если найдено окно со свойством Name == temp, то оно отображается а текущее закрывается
+                if (window.Name == "temp")
+                    window.Show();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ WPF_Demo/UnuformGridDemo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для UnuformGridDemo.xaml
+    /// </summary>
+    public partial class UnuformGridDemo : Window
+    {
+        public UnuformGridDemo()
+        {
+            InitializeComponent();
+            this.Name = "temp";
+        }
+
+        private void OpenMainWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+       
+    }
+}
